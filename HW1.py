0ef1820de87029cda8bf701944e662fbabe59965
Eden Xie <ex129247@student.wpsok.org>
2026-08-27 13:37:34 -0500

    Turned in HW2 and HW3

--- HW1.py
+++ HW1.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+# Name: Eden Xie
+# Class: 6th Hour
+# Assignment: HW1
+
+print("Hello World")
---
+++ HW2.py
@@ -0,0 +1,33 @@
+#Name: Eden Xie
+#Class: 6th Hour
+#Assignment: HW2
+
+print("Hello World")
+
+intVar1 = 4
+string_Var2 = "Box"
+bool_Var3 = True
+string_Var4 = "Nineteen"
+string_Var5 = "Rope"
+intVar6 = 19
+string_Var7 = "19"
+intVar8 = 2
+bool_Var9 = True
+bool_Var10 = False
+string_Var11 = "True"
+string_Var12 = "False"
+intVar13 = 5
+string_Var14 = "The"
+string_Var15 = "Game"
+bool_Var16  = False
+intVar17 = 3
+intVar18 = 2
+intVar19 = 1
+string_Var20 = string_Var11
+string_Var21 = string_Var12
+string_Var22 = string_Var4
+string_Var23 = string_Var14
+string_Var24 = string_Var15
+intVar25 = 6
+int_sum = intVar1 + intVar6 + intVar8 + intVar13 + intVar17 + intVar18 + intVar19 +intVar25
+print(int_sum)
